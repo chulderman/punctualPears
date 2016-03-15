4ae9d4c975d38fbdb830362721373773b6b2bc6b
Miguel Millan <Miguel Millan>
2016-03-15 11:05:47 -0400

    Complete JCFIteratorDemo.java

--- source/edu/oakland/homework/JCFIteratorDemo.java
+++ source/edu/oakland/homework/JCFIteratorDemo.java
@@ -4,7 +4,7 @@ import edu.oakland.helper.*;
 import java.util.*;
 
 /**
-*	This is the “manager” class with the “main” method
+*	This is the manager class with the main method
 *	that manages the other Java classes in the App; 
 *
 *	@author Chase Hulderman
@@ -12,9 +12,50 @@ import java.util.*;
 *	@version version 1.0 - 160216
 *	@since version 1.0
 */
-
 public class JCFIteratorDemo {
 	public JCFIteratorDemo(){
 	
 	}
+	
+	private int[] arrayRef;
+	private ArrayList<Integer> arrayListRef;
+	private SingleLinkedList linkListRef;
+	
+	
+	
+	public static void main(String[] args) {
+			ArrayGen aGen = new ArrayGen();
+			arrayRef = aGen.createArray();
+			
+			ArrayListGen aListGen = new ArrayListGen();
+			arrayListRef = aListGen.createArrayList(arrayRef);
+			
+			LinkedListGen LLGen = new LinkedListGen();
+			linkListRef = LLGen.createSingleLinkedList(arrayListRef);
+			
+			//Next create an AL<integer> iterator and find
+			//all odd numbers and print to cmd line
+			Iterator<Integer> iter1 = arrayListRef.iterator();
+			int tmp = 0;
+			while(iter1.hasNext()){
+				tmp = iter1.next();
+				if(tmp % 2 == 1)
+					System.out.println("Odd number found: " + tmp);
+			}
+			
+			//construct an iterator for a singleLinkedList 
+			//and find all even numbers
+			Iterator<SingleLinkedList> iter2 = linkListRef.iterator();
+			while(iter2.hasNext()){
+				tmp = iter2.next().getValue();
+				if(tmp % 2 == 1)
+					System.out.println("Odd number found: " + tmp);
+			}
+			
+			
+			
+			
+	}
+	
+	
 }
