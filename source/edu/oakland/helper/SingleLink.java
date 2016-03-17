14c365e6e87bec06c24fef17e90adb7dc02d5d3c
chulderman <chase@hulderman.com>
2016-03-17 10:24:13 -0400

    Added javadoc comments and completed overloaded constructor

--- source/edu/oakland/helper/SingleLink.java
+++ source/edu/oakland/helper/SingleLink.java
@@ -6,7 +6,7 @@ package edu.oakland.helper;
 *
 *	@author Chase Hulderman
 *	@author Miguel Millan
-*	@version version 1.0.1
+*	@version version 1.0.2
 *	@since version 1.0.0
 */
 
@@ -15,44 +15,117 @@ public class SingleLink {
 	private SingleLink nextLink;
 	private Integer value;
 
+	/**
+	*	Default class constructor - sets values to null.
+	*/
 	public SingleLink(){
 		this.value = null;
 		this.nextLink = null;
 	}
+
+	/**
+	*	Class constructor
+	*
+	*	@param	i	Set's link value by int
+	*/
 	public SingleLink(int i){
 		this.value = i;
 		this.nextLink = null;
 	}
+	
+	/**
+	*	Class constructor
+	*
+	*	@param	n	Sets link value by Integer
+	*/
 	public SingleLink(Integer n){
 		this.value = n;
 		this.nextLink = null;
 	}
+
+	/**
+	*	Class constructor
+	*
+	*	@param	link	Sets SingleLink's link reference
+	*/
+	public SingleLink(SingleLink link){
+		this.value = null;
+		this.nextLink = link;
+	}
+
+	/**
+	*	Class constructor
+	*
+	*	@param	i		Sets link value by int
+	*	@param	link	Sets SingleLink's link reference
+	*/
 	public SingleLink(int i, SingleLink link){
 		this.value = i;
 		this.nextLink = link;
 	}
+
+	/**
+	*	Class constructor
+	*
+	*	@param	i		Sets link value by Integer
+	*	@param	link	Sets SingleLink's link reference
+	*/
 	public SingleLink(Integer n, SingleLink link){
 		this.value = n;
 		this.nextLink = link;
 	}
 
+	// Begin Mutators //
+	
+	/**
+	*	Sets (or re-sets) the link's new link reference.
+	*	
+	*	@param	e	SingleLink reference address
+	*/
 	public void setNext(SingleLink e){
 		this.nextLink = e;
 		return;
 	}
+
+	/**
+	*	Sets (or re-sets) the link value by recieving an Integer.
+	*	
+	*	@param	n	Recieves Integer and sets (or re-sets) the SingleLink value
+	*/
 	public void setValue(Integer n){
 		this.value = n;
 		return;
 	}
+
+	/**
+	*	Overloaded method. Sets (or re-sets) the link value by converting an int to Integer
+	*	
+	*	@param	i	Recieves int and sets SingleLink value as Integer. 
+	*/
 	public void setValue(int i){
 		this.value = new Integer(i);
 		return;
 	}
+	// End Mutators //
+
+	// Begin Accessors //
+
+	/**
+	*	Retrieves the next link.
+	*	
+	*	@return	Returns the reference for the next SingleLink link.
+	*/
 	public SingleLink getNext(){
 		return this.nextLink;
 	}
+
+	/**
+	*	Retrieves link value.
+	*	
+	*	@return	returns link value as an Integer
+	*/
 	public Integer getValue(){
 		return this.value;
 	}
-
+	// End Accessors //
 }
