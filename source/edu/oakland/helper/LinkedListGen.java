66ebe7d07465c728df878d14ce415cd663715b2f
chulderman <chase@hulderman.com>
2016-03-17 11:22:22 -0400

    Implemented a basic iterator

--- source/edu/oakland/helper/LinkedListGen.java
+++ source/edu/oakland/helper/LinkedListGen.java
@@ -1,16 +1,16 @@
 package edu.oakland.helper;
 
 /**
-*	This class is responsible for generating the SinglyLinkedList<SingleLink> from
+*	This class is responsible for generating the LinkedList<SingleLink> from
 *	SinglLinks created by the SingleLink class;
 *
 *	@author Chase Hulderman
 *	@author Miguel Millan
-*	@version version 1.0.0
+*	@version version 1.0.1
 *	@since version 1.0.0
 */
 
-public class LinkedListGen extends LinkedList<SingleLink> {
+public class LinkedListGen{
 
 	private SingleLink currentLink;
 
@@ -24,10 +24,37 @@ public class LinkedListGen extends LinkedList<SingleLink> {
 	public boolean isEmpty(){
 		return (this.currentLink = null);
 	}
+	public LinkedListIterator listIterator(){
+		LinkedListIterator iter = new LinkedListIterator(this.currentLink);
+		return iter;
+	}
 	public void insertLink(Integer n, SingleLink link){
 		SingleLink newLink = new SingleLink(n, link);
 		newLink.setNext(currentLink);
 		this.currentLink = newLink;
 		return;
+	}	
+	public LinkedListGen createSingleLinkedList(ArrayList<Integer> integerArray){
+		LinkedListGen newList = new LinkedListGen();
+		// Fill linked list with the integer array
+		return newList;
+	}
+
+	private class LinkedListIterator extends ListIterator<SingleLink>{
+		private SingleLink ref;
+		private LinkedListIterator(SingleLink link){
+			this.ref = link;
+		}
+		public boolean hasNext(){
+			return (ref != null);
+		}
+		public Integer next(){
+			if(ref.getNext() == null) {
+				return null;
+			}
+			Integer value = ref.getValue();
+			this.ref = ref.getNext();
+			return value;
+		}
 	}
 }
