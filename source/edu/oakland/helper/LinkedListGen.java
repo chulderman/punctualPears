e2c500e1cd11f9a0d5d3d2fb10e62a4a4d904857
chulderman <chase@hulderman.com>
2016-03-17 10:36:30 -0400

    initial implementation.

--- source/edu/oakland/helper/LinkedListGen.java
+++ source/edu/oakland/helper/LinkedListGen.java
@@ -6,12 +6,28 @@ package edu.oakland.helper;
 *
 *	@author Chase Hulderman
 *	@author Miguel Millan
-*	@version version 1.0 - 160216
-*	@since version 1.0
+*	@version version 1.0.0
+*	@since version 1.0.0
 */
 
-public class LinkedListGen {
+public class LinkedListGen extends LinkedList<SingleLink> {
+
+	private SingleLink currentLink;
+
+	/**
+	*	Default class constructor - sets values to null.
+	*/
 	public LinkedListGen(){
-	
+		this.currentLink = null;
+	}
+
+	public boolean isEmpty(){
+		return (this.currentLink = null);
+	}
+	public void insertLink(Integer n, SingleLink link){
+		SingleLink newLink = new SingleLink(n, link);
+		newLink.setNext(currentLink);
+		this.currentLink = newLink;
+		return;
 	}
 }
