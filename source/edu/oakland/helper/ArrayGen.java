0c9399a097e100fa1e773ce6386c7ee791483bb9
Miguel Millan <Miguel Millan>
2016-03-17 10:47:09 -0400

    Finish ArrayGen Class

--- source/edu/oakland/helper/ArrayGen.java
+++ source/edu/oakland/helper/ArrayGen.java
@@ -12,7 +12,37 @@ package edu.oakland.helper;
 */
 
 public class ArrayGen {
+	private int[] arr;
+	private int arraySize;
+	
 	public ArrayGen(){
+		this(20);
+	}
+	
+	public ArrayGen(int size){
+		this.arraySize = size;
+		this.arr = new int[arraySize];
+	}
 
+	public int[] createArray(int high){
+		for(int i=0; i<arraySize;i++)
+			this.arr[i] = (int)(high*Math.random() + 1);
+		
+		return this.arr;
+	}
+	
+	public int[] createArray(){
+		for(int i=0; i<arraySize;i++)
+			this.arr[i] = (int)(20*Math.random() + 1);
+		
+		return this.arr;
+	}
+	
+	public static void main(String[] args) {
+		ArrayGen aGen = new ArrayGen();
+		int[] arr1 = aGen.createArray();
+		
+		for(int i=0; i<arr1.length; i++)
+			System.out.print(arr1[i] + " ");
 	}
 }
